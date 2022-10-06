7501aa0ad661524c623ab369eaea3767a8704ff3
jdbacero <jdbacero@gmail.com>
2022-10-07 01:24:13 +0800

    2.11

---
+++ part 2/2.2-2.5/src/App.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './component/Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promised fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const note = {
+      content: newNote,
+      date: new Date().toISOString,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+    setNotes(notes.concat(note))
+    setNewNote('')
+    console.log('Submit clicked.', e.target)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "Important" : "All"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
